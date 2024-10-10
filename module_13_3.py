489080f09339363c8d5512c2551fcf218c43283d
Dmitry <hulk295@yandex.ru>
2024-10-10 17:25:00 +0300

    Настройка бота - добавлена обработка "Он мне ответил!"

---
+++ module_13_3.py
@@ -0,0 +1,27 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = '8116457190:AAH6z1ebWXcoD4s-zXk1Xk0YWVNPMKT52Io'
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(text=['Urban', 'ff'])
+async def urban_message(message):
+    await message.answer('urban message')
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью. Рад вас видеть!')
+
+# @dp.message_handler(commands=['start'])
+# async def start_message(message):
+#     print('Start message')
+
+
+@dp.message_handler()
+async def all_message(message):
+    await message.answer("Не понимаю(( Введите команду /start, чтобы начать общение.")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
