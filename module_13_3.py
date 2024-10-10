024f0a77bf76033fefbae07ad0ca979ad938ccda
Dmitry <hulk295@yandex.ru>
2024-10-10 17:25:28 +0300

    Настройка бота - добавлена обработка "Он мне ответил!"

--- module_13_3.py
+++ module_13_3.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = '8116457190:AAH6z1ebWXcoD4s-zXk1Xk0YWVNPMKT52Io'
+api = 'KEY'
 bot = Bot(token = api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
