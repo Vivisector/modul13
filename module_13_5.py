4ca4b86bcc57702fc7b1923e6b71b8af94188dca
Dmitry <hulk295@yandex.ru>
2024-10-13 19:03:20 +0300

    Домашнее задание по теме "Клавиатура кнопок".

---
+++ module_13_5.py
@@ -0,0 +1,61 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+bot = Bot(token='KEY')
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button1 = KeyboardButton(text = 'Рассчитать')
+button2 = KeyboardButton(text = 'Информация')
+kb.add(button1)
+kb.add(button2)
+# kb.row(button1, button2)
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+@dp.message_handler(commands='start')
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.\nДля начала расчета нажмите кнопку "Рассчитать"', reply_markup = kb)
+
+@dp.message_handler(text='Информация')
+async def info(message):
+    await message.answer('Under construction')
+@dp.message_handler(text='Рассчитать')
+async def cmd_start(message: types.Message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+@dp.message_handler(state=UserState.age)
+async def process_age(message: types.Message, state: FSMContext):
+    await state.update_data(age=int(message.text))
+    await message.answer('Введите свой рост (в см):')
+    await UserState.next()
+
+@dp.message_handler(state=UserState.growth)
+async def process_growth(message: types.Message, state: FSMContext):
+    await state.update_data(growth=int(message.text))
+    await message.answer('Введите свой вес (в кг):')
+    await UserState.next()
+
+@dp.message_handler(state=UserState.weight)
+async def process_weight(message: types.Message, state: FSMContext):
+    await state.update_data(weight=int(message.text))
+    data = await state.get_data()
+
+    # Формула для мужчин
+
+    norma = 10 * data['weight'] + 6.25 * data['growth'] - 5 * data['age'] + 5
+
+    await message.answer(f"Ваша суточная норма калорий составляет примерно:\n{round(norma)} ккал")
+    await message.answer(f"Для нового расчета\nнажмите кнопку 'Рассчитать'")
+    await state.finish()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
